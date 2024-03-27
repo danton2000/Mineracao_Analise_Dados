c1602b82cd4c8aac765c95343e616b89b79cc6c4
DANTON DE CAMPOS RODRIGUES <181810032@fspoa.br>
2024-03-26 21:58:50 -0300

    Finalizando ativ 1

--- aula03/ex1-2.py
+++ aula03/ex1-2.py
@@ -13,46 +13,109 @@
 # soma = 0
 # produto = ""
 
+# lista para valores, maior e menor
+
 # tem a fruta? não, coloca na nova lista, se tiver validar os valores
 
 # Importa a lib para utilizar e manipular arquivos .CSV
 import csv
 
 # Pegando o arquivo utilizando o caminho absuluto
-arquivo = open("./aula03/cadastro_produtosV1.csv")
+try:
+    arquivo = open("./aula03/cadastro_produtosV1.csv")
+except:
+    print("Falha ao encontrar o arquivo.")
+    exit()
 
 # Colocando todas as informações do arquivo na variavel
 linhas = csv.reader(arquivo)
 
-maior_preco = 0
-menor_preco = 0
+lista_maior_preco = []
+lista_menor_preco = []
+lista_qtd_frutas = []
+qtd = 1
+lista_media_preco_frutas = []
+lista_valor_total_frutas = []
+lista_qtd_fruta_acima_media = []
+
+lista_frutas = []
+
 soma = 0
-qtd_produto_acima_media = 0
-lista_valores = []
-nome_fruta = ""
-lista_frutas_selecionados = []
-lista_frutas_valores = []
-
-
 cursor = 0
 
 for linha in linhas:
 
     if cursor > 0:
-        
-        nome_fruta = linha[1]
-        valor = float(linha[2])
-        
-        if nome_fruta not in lista_frutas_selecionados:
+
+        if linha[1] not in lista_frutas:
             
-            lista_frutas_selecionados.append(nome_fruta)
+            lista_frutas.append(linha[1])
+
+            lista_maior_preco.append(float(linha[2]))
+
+            lista_menor_preco.append(float(linha[2]))
+
+            lista_qtd_frutas.append(qtd)
+
+            lista_valor_total_frutas.append(0)
+
+            lista_media_preco_frutas.append(0)
+
+            lista_qtd_fruta_acima_media.append(0)
             
-            lista_frutas_valores.append([nome_fruta, valor])
-      
+        else:
+            
+            # encontrado a fruta que foi repetido
+            index_fruta = lista_frutas.index(linha[1])
+
+            # Verificando o maior valor das frutas
+            if float(linha[2]) > lista_maior_preco[index_fruta]:
+                # Atualizando as informações pelo indice encontrado
+                lista_frutas[index_fruta] = linha[1]
+
+                lista_maior_preco[index_fruta] = float(linha[2])
+
+                soma += float(linha[2])
+
+                lista_valor_total_frutas[index_fruta] = soma
+
+            # Verificando o maior valor das frutas
+            if float(linha[2]) < lista_menor_preco[index_fruta]:
+                # Atualizando as informações pelo indice encontrado
+                lista_frutas[index_fruta] = linha[1]
+
+                lista_menor_preco[index_fruta] = float(linha[2])
+            
+            # Verificando a qtd de frutas que foram repitidas
+            lista_qtd_frutas[index_fruta] += 1
+
+            # Verificando a media de preços das frutas
+            #soma = float(linha[2])
+
+            lista_media_preco_frutas[index_fruta] = lista_valor_total_frutas[index_fruta] / lista_qtd_frutas[index_fruta] 
+
+            #Verificando a qtd de frutas que estão acima da media
+            if float(linha[2]) > lista_media_preco_frutas[index_fruta]:
+
+                lista_qtd_fruta_acima_media[index_fruta] += 1
+
+
+            
     cursor += 1
 
+print(lista_frutas[13])
 
-print(lista_frutas_valores)
+print(lista_maior_preco[13])
+
+print(lista_menor_preco[13])
+
+print(lista_qtd_frutas[13])
+
+print(lista_valor_total_frutas[13])
+
+print(lista_media_preco_frutas[13])
+
+print(lista_qtd_fruta_acima_media[13])
     
 
     
