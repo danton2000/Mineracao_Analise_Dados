80fa457cf67649ef47d1a1d34f5e28fa1373d155
Danton Rodrigues <dantoncampos@hotmail.com>
2024-03-31 11:34:29 -0300

    Finalizando o trabalho

--- aula03/ex1-2.py
+++ aula03/ex1-2.py
@@ -7,16 +7,6 @@
 # O valor médio deste produto;
 # A quantidade deste produto que apresenta valor acima da média.
 
-# maior = 0
-# menor = 0
-# qtd = 0
-# soma = 0
-# produto = ""
-
-# lista para valores, maior e menor
-
-# tem a fruta? não, coloca na nova lista, se tiver validar os valores
-
 # Importa a lib para utilizar e manipular arquivos .CSV
 import csv
 
@@ -26,27 +16,50 @@ try:
 except:
     print("Falha ao encontrar o arquivo.")
     exit()
-
+       
 # Colocando todas as informações do arquivo na variavel
 linhas = csv.reader(arquivo)
 
+# Variaveis de inicialização
 lista_maior_preco = []
 lista_menor_preco = []
 lista_qtd_frutas = []
-qtd = 1
 lista_media_preco_frutas = []
 lista_valor_total_frutas = []
 lista_qtd_fruta_acima_media = []
-
+lista_valor_frutas = []
 lista_frutas = []
-
-soma = 0
 cursor = 0
+
+def verificaMaiorValorFruta(valor_fruta, nome_fruta, index_fruta):
+    if valor_fruta > lista_maior_preco[index_fruta]:
+        # Atualizando as informações pelo indice encontrado
+        lista_frutas[index_fruta] = nome_fruta
+
+        lista_maior_preco[index_fruta] = valor_fruta
+        
+def verificaMenorValorFruta(valor_fruta, nome_fruta, index_fruta):
+    if valor_fruta < lista_menor_preco[index_fruta]:
+        # Atualizando as informações pelo indice encontrado
+        lista_frutas[index_fruta] = nome_fruta
+
+        lista_menor_preco[index_fruta] = valor_fruta
+
+def verificaQuantidadeFruta(index_fruta):
+    lista_qtd_frutas[index_fruta] += 1
+
+def VerificaValorTotalFruta(index_fruta):
+    lista_valor_total_frutas[index_fruta] += float(linha[2])
+
+def verificaMediaFruta(index_fruta):
+    lista_media_preco_frutas[index_fruta] = lista_valor_total_frutas[index_fruta] / lista_qtd_frutas[index_fruta] 
+
 
 for linha in linhas:
 
     if cursor > 0:
 
+        # Se o nome da fruta não estiver na lista de frutas
         if linha[1] not in lista_frutas:
             
             lista_frutas.append(linha[1])
@@ -55,67 +68,73 @@ for linha in linhas:
 
             lista_menor_preco.append(float(linha[2]))
 
-            lista_qtd_frutas.append(qtd)
-
-            lista_valor_total_frutas.append(0)
+            lista_qtd_frutas.append(1)
+    
+            lista_valor_total_frutas.append(float(linha[2]))
 
             lista_media_preco_frutas.append(0)
-
-            lista_qtd_fruta_acima_media.append(0)
             
+            lista_valor_frutas.append(float(linha[2]))   
+
+            lista_qtd_fruta_acima_media.append(0)   
         else:
             
             # encontrado a fruta que foi repetido
             index_fruta = lista_frutas.index(linha[1])
-
+            
+            VerificaValorTotalFruta(index_fruta)
+            
             # Verificando o maior valor das frutas
-            if float(linha[2]) > lista_maior_preco[index_fruta]:
-                # Atualizando as informações pelo indice encontrado
-                lista_frutas[index_fruta] = linha[1]
+            verificaMaiorValorFruta(float(linha[2]), linha[1], index_fruta)
 
-                lista_maior_preco[index_fruta] = float(linha[2])
-
-                soma += float(linha[2])
-
-                lista_valor_total_frutas[index_fruta] = soma
-
-            # Verificando o maior valor das frutas
-            if float(linha[2]) < lista_menor_preco[index_fruta]:
-                # Atualizando as informações pelo indice encontrado
-                lista_frutas[index_fruta] = linha[1]
-
-                lista_menor_preco[index_fruta] = float(linha[2])
+            # Verificando o menor valor das frutas
+            verificaMenorValorFruta(float(linha[2]), linha[1], index_fruta)
             
             # Verificando a qtd de frutas que foram repitidas
-            lista_qtd_frutas[index_fruta] += 1
-
+            verificaQuantidadeFruta(index_fruta)
+            
             # Verificando a media de preços das frutas
-            #soma = float(linha[2])
-
-            lista_media_preco_frutas[index_fruta] = lista_valor_total_frutas[index_fruta] / lista_qtd_frutas[index_fruta] 
+            verificaMediaFruta(index_fruta)
 
             #Verificando a qtd de frutas que estão acima da media
-            if float(linha[2]) > lista_media_preco_frutas[index_fruta]:
-
-                lista_qtd_fruta_acima_media[index_fruta] += 1
-
-
-            
+            # Não consegui fazer ...
+                 
     cursor += 1
 
-print(lista_frutas[13])
+arquivo.close()
 
-print(lista_maior_preco[13])
+# Pegando o arquivo utilizando o caminho absuluto
+try:
+    arquivo2 = open("./aula03/cadastro_produtosV1.csv")
+except:
+    print("Falha ao encontrar o arquivo.")
+    exit()
+       
+# Colocando todas as informações do arquivo na variavel
+linhas2 = csv.reader(arquivo2)
 
-print(lista_menor_preco[13])
+for linha2 in linhas2:
+    
+    if cursor > 0:
+        
+        pass
+    
+    
+    cursor += 1
+    
+# print(lista_frutas[13])
 
-print(lista_qtd_frutas[13])
+# print(lista_maior_preco[13])
 
-print(lista_valor_total_frutas[13])
+# print(lista_menor_preco[13])
 
-print(lista_media_preco_frutas[13])
+# print(lista_qtd_frutas[13])
 
-print(lista_qtd_fruta_acima_media[13])
+# print(lista_valor_total_frutas[13])
+
+# print(lista_media_preco_frutas[13])
+
+# print(lista_qtd_fruta_acima_media[13])
     
 
     
