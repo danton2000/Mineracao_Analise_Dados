8ecc8fd1162dd82c0f56141efe12f508c7e6d96f
DANTON DE CAMPOS RODRIGUES <181810032@fspoa.br>
2024-06-25 20:54:14 -0300

    Subindo trabalho 4

--- exercicio04/top_jogadores.py
+++ exercicio04/top_jogadores.py
@@ -7,17 +7,19 @@ url = 'https://raw.githubusercontent.com/danton2000/Mineracao_Analise_Dados/main
 
 # Ler o csv e criando um data frame
 dataframe = pd.read_csv(url)
+print(f"Linhas extraidas {len(dataframe.index)}.")
 
-# Imprimindo os dados
-#print(dataframe['Player Names'])
 # TRANSFORMAÇÃO
 # Ordenando o dataframe pela coluna 'Goals'(quantidade de gols marcados)
 dataframeordenado = dataframe.sort_values(by='Goals', ascending=False)
 
 # Pegando os 10 jogadores que mais marcaram gols 
-goleadores = dataframeordenado[['Player Names', 'Club', 'Goals', 'Year']].head(10)
+goleadores = dataframeordenado[['Player Names', 'Club', 'Goals', 'Year']].head(12)
+print("Tratamento/Manipulção foi realizado.")
 
 # CARREGAMENTO
-# Salvando e criando um arquivo com informações de um outro arquivo .csv
-output_file_path = 'exercicio04/top_10_goleadores_2016_2020.csv'
+# Salvando e criando um arquivo com as informações para um outro arquivo .csv
+output_file_path = 'top_10_goleadores_2016_2020.csv'
 goleadores.to_csv(output_file_path, index=False)
+
+print("Arquivo criado.")
